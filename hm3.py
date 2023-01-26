4daabbae93492474f9c250e08980e93838e319c4
ashatkydyrov61@gmail.com <ashatkydyrov61@gmail.com>
2023-01-26 14:00:42 -0600

    first commit

--- hm3.py
+++ hm3.py
@@ -8,10 +8,20 @@ class Bank:
         return f"на вашем счёте: {self.__balance}, Имя влодельца: {self.__name}"
     def _kill(self):
         user = int(input("сколько вы хотите обноличить: "))
-        self.__balance -= user
-        return f"на вашем счёте: {self.__balance}"
-    def __jackpot(self):
-        self.__balance * 10
-        return f"на вашем счёте: {self.__balance}"
-bank = Bank("askhat")
-print(bank.money_x())
+        if user <= 0:
+            self.__balance -= user
+            return f"на вашем счёте: {self.__balance}"
+        else:
+            return f"на вашем счете не хвотает денег, на счете: {self.__balance}"
+    def __jacpot(self):
+        return f"ваш счет ужножок на 10, ваш счет: {self.__balance * 10}"
+    def user(self,name,__balanse):
+        self.name = name
+        self.__balanse = __balanse
+        return f"Имя: {self.name}, Balanse: {self.__balanse + self.__balance}"
+ban = Bank("askhat")
+print(ban.money_x())
+print(ban._kill())
+print(ban.user("bekbolot", 100))
+print(ban._Bank__jacpot())
+
